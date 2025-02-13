4516aa7411cfb91873048f308e7fa98cb4c2345f
Brayan Jimenez <brianjim24@gmail.com>
2025-02-12 21:25:13 -0500

    classs N/1

--- Link a js/First-exercise.js
+++ Link a js/First-exercise.js
@@ -1,9 +1,8 @@
-let numeros = [1, 2, 3, 4, 5];
-let suma = 0;
-numeros.forEach (numero => {
-    suma += numero;
-    
-    console.log(suma);
+let numeros = [1,2,3,4,5]
+let resultado = 0;
+numeros.forEach(numero => {
+    resultado += numero; 
+    console.log(resultado);
 });
    
     
