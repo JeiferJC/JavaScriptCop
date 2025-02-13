03e9a98a12b4b5c501ebb6d6a9e375eb2dbc89b5
Dainover <jimenezelkin02@gmail.com>
2025-02-13 11:41:11 -0500

    3.0

---
+++ Spotify/L.js
@@ -0,0 +1,16 @@
+// scripts.js
+
+function playPause() {
+    // Aquí iría la lógica para reproducir/pausar la canción
+    alert('Reproducir/Pausar');
+}
+
+function previousSong() {
+    // Aquí iría la lógica para la canción anterior
+    alert('Canción anterior');
+}
+
+function nextSong() {
+    // Aquí iría la lógica para la siguiente canción
+    alert('Siguiente canción');
+}
